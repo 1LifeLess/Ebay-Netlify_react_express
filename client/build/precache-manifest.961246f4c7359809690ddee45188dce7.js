0899888c6d05d0854496c9f2abe414f3cfaa1da1
1LifeLess <tomersss2@gmail.com>
2020-12-20 14:05:55 +0200

    build

---
+++ client/build/precache-manifest.961246f4c7359809690ddee45188dce7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3c19d51c0999bf70469dc61828c0cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bbc52e70a0aaaecca34",
+    "url": "/static/css/2.c72968ad.chunk.css"
+  },
+  {
+    "revision": "3b1948d69a2bd306816b",
+    "url": "/static/css/main.1e91eac5.chunk.css"
+  },
+  {
+    "revision": "2bbc52e70a0aaaecca34",
+    "url": "/static/js/2.355f1867.chunk.js"
+  },
+  {
+    "revision": "1fb97997b0ecd7d7d88e3d0a6b6bb983",
+    "url": "/static/js/2.355f1867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1948d69a2bd306816b",
+    "url": "/static/js/main.bf440692.chunk.js"
+  },
+  {
+    "revision": "9be6ff1146c85359aa3b",
+    "url": "/static/js/runtime-main.a0e0afc4.js"
+  },
+  {
+    "revision": "38123ae2a6aa9a9dbfa1f5816767cdbf",
+    "url": "/static/media/ebay.38123ae2.jpg"
+  }
+]);
