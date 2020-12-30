fb3f4256a65129939d80bcfc44e5362f4294b2bb
1LifeLess <tomersss2@gmail.com>
2020-12-30 18:23:41 +0200

    build done

---
+++ client/build/precache-manifest.2cd32a9843ae416b46b96f5fc820a138.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043b7367e47b11323ea52bda7ef043e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3dc53fd6c2873aa0057",
+    "url": "/static/css/2.c72968ad.chunk.css"
+  },
+  {
+    "revision": "bc4f41ba975fbf4fb3b6",
+    "url": "/static/css/main.509763c5.chunk.css"
+  },
+  {
+    "revision": "c3dc53fd6c2873aa0057",
+    "url": "/static/js/2.83d4d13f.chunk.js"
+  },
+  {
+    "revision": "1fb97997b0ecd7d7d88e3d0a6b6bb983",
+    "url": "/static/js/2.83d4d13f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc4f41ba975fbf4fb3b6",
+    "url": "/static/js/main.accb95ae.chunk.js"
+  },
+  {
+    "revision": "9be6ff1146c85359aa3b",
+    "url": "/static/js/runtime-main.a0e0afc4.js"
+  },
+  {
+    "revision": "38123ae2a6aa9a9dbfa1f5816767cdbf",
+    "url": "/static/media/ebay.38123ae2.jpg"
+  }
+]);
