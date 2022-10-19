126d51e2638791e58b14e3d4c5edb86ff6fad0b9
1LifeLess <tomersss2@gmail.com>
2022-10-19 23:48:15 +0300

    update welcome
    chnages password

--- client/src/components/welcome.js
+++ client/src/components/welcome.js
@@ -26,7 +26,7 @@ export default function Welcome() {
   <Modal.Body>
     <p> This project use Ebay API service,<br />
     According to Ebay policy and for security reasons, you will have to login to Ebay, <br/>
-   Please copy the following credentials or use your own if you already have an Ebay account</p>
+   Please copy the following login or use your own if you already have an Ebay account</p>
    UserName: <span style={{fontWeight:'bold'}}>reactappebay@gmail.com</span><br />
  password:  <span style={{fontWeight:'bold'}}>A!2345678</span>
 
